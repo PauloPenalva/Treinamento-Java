fc1025a5c5d0011fec9227363991ce7493d9f101
paulopenalva <paulo.penalva@gmail.com>
2022-09-21 13:30:23 -0300

    first commit

---
+++ Aula00/aula00/src/Aula00.java
@@ -0,0 +1,12 @@
+public class Aula00 {
+
+    public static void main(String[] args) {
+        String message = printMessage();
+        System.out.println(message);
+    }
+    
+    private static String printMessage() {
+        String message = "Ola, mundo !";
+        return message;
+    }
+}
